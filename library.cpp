b8131acee077a7fe28f6a3128f8563c7946c4087
tiffanyteng1 <64996570+tiffanyteng1@users.noreply.github.com>
2020-11-19 19:26:25 -0500

    Add files via upload

---
+++ library.cpp
@@ -0,0 +1,22 @@
+//
+//  library.cpp
+//  a5
+//
+//  Created by Tiffany Teng on 2020-11-19.
+//  Copyright © 2020 Tiffany Teng. All rights reserved.
+//
+
+#include "library.hpp"
+#include "book.hpp"
+
+Library:: Library(){
+    
+}
+
+Library::Library(vector<book>new_books):
+    my_books(new_books){
+    }
+
+void store_books(vector<book>my_books){
+    
+}
